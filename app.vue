c0d3f664f4b29ca7b122aee82b5aca4fb5ef8c81
Petro Silenius <petro.silenius@gmail.com>
2023-02-06 23:55:37 +0200

    Move globale styles to app.vue

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  color: #f8f4e3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -91,12 +91,3 @@ h1 {
   );
 }
 </style>
-
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Roboto', sans-serif;
-  color: #f8f4e3;
-}
-</style>
