20c625a61ae849c278cb52d7cd4c7b0a06c9319c
Petro Silenius <petro.silenius@gmail.com>
2023-02-06 23:55:12 +0200

    Call openAI APIs to generate the statement and matching image

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,70 @@
 <script setup lang="ts">
 import Aphorism from '../components/Aphorism.vue';
 
-const aphorism = "Don't be afraid to fail";
-const imageUrl = 'https://source.unsplash.com/random/500x420';
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: 'Bearer ' + process.env.OPENAI_API_KEY,
+};
+
+const { data: texts, error } = await useFetch<{
+  choices: [
+    {
+      text: string;
+      index: number;
+      logprobs: null;
+      finish_reason: string;
+    }
+  ];
+}>('https://api.openai.com/v1/completions', {
+  headers,
+  method: 'POST',
+  body: JSON.stringify({
+    prompt: 'Write a short inspirational goal-oriantated aphorism for a startup',
+    max_tokens: 5,
+    temperature: 0.9,
+    model: 'text-ada-001',
+  }),
+});
+
+console.log({ texts, error });
+
+const aphorism = texts?.value?.choices[0].text;
+if (!aphorism) {
+  throw new Error('No aphorism found');
+}
+
+const { data: images } = await useFetch<{ data: [{ url: string }] }>(
+  'https://api.openai.com/v1/images/generations',
+  {
+    headers,
+    method: 'POST',
+    body: JSON.stringify({
+      prompt:
+        'A slightly blurred background image with a thoughtful environment that matches the aphorism' +
+        aphorism,
+      n: 1,
+      size: '500x420',
+    }),
+  }
+);
+
+console.log({ images });
+
+const imageUrl = images?.value?.data[0].url;
 </script>
 
 <template>
-  <div>
-    <div class="grid">
-      <div>
-        <h1>"Write an aphorism for a startup that fits with a nice background image"</h1>
-        <Aphorism :aphorism="aphorism" :imageUrl="imageUrl" />
-      </div>
-      <div>
-        <h2>What is startup-aphorisms?</h2>
-        <p>
-          Startup-aphorisms is a project that uses OpenAI's API to generate generic aphorisms about
-          startup culture.
-        </p>
-      </div>
+  <div class="grid">
+    <div>
+      <h1>"Write an aphorism for a startup that fits with a nice background image"</h1>
+      <Aphorism :aphorism="aphorism" :imageUrl="imageUrl" />
+    </div>
+    <div>
+      <h2>What is startup-aphorisms?</h2>
+      <p>
+        Startup-aphorisms is a project that uses OpenAI's API to generate generic aphorisms about
+        startup culture.
+      </p>
     </div>
   </div>
 </template>
