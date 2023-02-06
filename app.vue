e87afd66f90b87ba4829b6691863186a86e9e3ef
Petro Silenius <petro.silenius@gmail.com>
2023-02-06 23:35:04 +0200

    Create layout and initial content for index page

--- app.vue
+++ app.vue
@@ -1,5 +1,56 @@
+<script setup lang="ts">
+import Aphorism from './components/Aphorism.vue';
+
+const aphorism = "Don't be afraid to fail";
+const imageUrl = 'https://source.unsplash.com/random/500x420';
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="grid">
+      <div>
+        <h1>"Write an aphorism for a startup that fits with a nice background image"</h1>
+        <Aphorism :aphorism="aphorism" :imageUrl="imageUrl" />
+      </div>
+      <div>
+        <h2>What is startup-aphorisms?</h2>
+        <p>
+          Startup-aphorisms is a project that uses OpenAI's API to generate generic aphorisms about
+          startup culture.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+  font-weight: 600;
+  text-shadow: 0 0 2px #000;
+  margin-bottom: 6rem;
+}
+
+.grid {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  padding: 0 15px;
+  text-align: center;
+  background: linear-gradient(
+    45deg,
+    hsla(333, 37%, 46%, 1) 10%,
+    hsla(243, 10%, 47%, 1) 50%,
+    hsla(197, 44%, 42%, 1) 100%
+  );
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  color: #f8f4e3;
+}
+</style>
---
+++ components/Aphorism.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    aphorism: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <img :src="imageUrl" width="500" height="420" />
+    <p class="aphorism">{{ aphorism }}</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+}
+
+img {
+  width: 100%;
+  max-width: 500px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px #000;
+  object-fit: cover;
+}
+
+.aphorism {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+  font-style: italic;
+  text-shadow: 0 0 2px #000;
+}
+</style>
